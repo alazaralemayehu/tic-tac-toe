390b332d67764bb3b5501f6d692fa0dc0d297b39
alazaralemayehu <alazar.1994@gmail.com>
2022-06-02 13:53:35 +0300

    updated bash file

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-exec docker build -t flask-application .
-exec docker run -p 5000:5000 -d flask-application
+sudo docker build -t flask-application .
+sudo docker run -p 5000:5000 -d flask-application
